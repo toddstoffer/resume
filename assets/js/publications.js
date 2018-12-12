51347816b67f2f4c50d79532c91927b36e023637
Todd Stoffer <tdstoffe@ncsu.edu>
2018-12-12 14:25:05 -0500

    initial commit

---
+++ assets/js/publications.js
@@ -0,0 +1,28 @@
+
+var request = new XMLHttpRequest();
+request.open('GET', '/assets/api.json', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.citations.forEach(citations => {
+
+        var link
+
+        var div = document.createElement("div");
+        div.setAttribute("id", "item")
+        div.innerHTML = "<div class='upper-row'><h3 class='presentation-title'><a href='" + citations.source.doi + "'>" + citations.title + "</a></h3>" + "<div class='pub-time'>" + citations.year + "</div></div>" + "<p class='presentation-summary'>" + citations.author + "</p>"
+
+        document.getElementById('container').appendChild(div)
+
+    });
+
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.innerHTML = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
